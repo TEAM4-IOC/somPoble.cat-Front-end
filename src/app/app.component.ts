75a54ef489dbc837453e123324af1d7a75feb40a
Miriam <miri@paf1.com>
2025-03-10 20:37:07 +0100

    Instalación componente header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { I18nService } from './core/services/i18n.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TranslateModule],
+  imports: [RouterOutlet, TranslateModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  constructor(private i18nService: I18nService) {}
-
-  changeLanguage(lang: string) {
-    this.i18nService.changeLanguage(lang);
-  }
+  
 }
---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { I18nService } from '../../core/services/i18n.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+  constructor(private i18nService: I18nService) {}
+
+  changeLanguage(lang: string) {
+    this.i18nService.changeLanguage(lang);
+  }
+}
