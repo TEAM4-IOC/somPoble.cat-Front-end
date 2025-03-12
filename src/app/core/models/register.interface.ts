1880372d54e4aecd9a96d37213dd95b72117fcc3
Daniel <danideveloperweb@gmail.com>
2025-03-12 09:49:54 +0100

    interfaces

---
+++ src/app/core/models/register.interface.ts
@@ -0,0 +1,11 @@
+export interface RegisterRequest {
+  idPersona: number;
+  dni: string;
+  nombre: string;
+  apellidos: string;
+  email: string;
+  telefono: string;
+  contraseña: string;
+  fechaAlta: string;
+  fechaModificacion: string;
+}
