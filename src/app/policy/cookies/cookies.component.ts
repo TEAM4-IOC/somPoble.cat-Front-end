d3a901a0b7badbd2e7dcd7b24236857e588e9f27
Miriam <miri@paf1.com>
2025-03-12 22:38:07 +0100

    Texto Cookies + traducciones

--- src/app/policy/cookies/cookies.component.ts
+++ src/app/policy/cookies/cookies.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cookies',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule, TranslateModule],
   templateUrl: './cookies.component.html',
   styleUrl: './cookies.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
