d89cea6db1fcb14eeaea7e474f315e849d5eb5ec
Miriam <miri@paf1.com>
2025-03-13 14:59:29 +0100

    subida parcial texto privacy

--- src/app/policy/privacy/privacy.component.ts
+++ src/app/policy/privacy/privacy.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule, TranslateModule],
   templateUrl: './privacy.component.html',
   styleUrl: './privacy.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
