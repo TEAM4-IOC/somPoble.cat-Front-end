69addd23a9d281ec7ac797d50dae2a3e9c99bca3
Miriam <miri@paf1.com>
2025-03-13 23:09:19 +0100

    Subida completa de textos legales

--- src/app/policy/legal-notice/legal-notice.component.ts
+++ src/app/policy/legal-notice/legal-notice.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule, TranslateModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
