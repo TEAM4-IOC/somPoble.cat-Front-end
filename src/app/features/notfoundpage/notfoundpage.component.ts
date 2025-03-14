249397e34e0465b937b6ab2586612361bee6214e
Miriam <miri@paf1.com>
2025-03-14 20:32:11 +0100

    Traducciones añadidas página 404

--- src/app/features/notfoundpage/notfoundpage.component.ts
+++ src/app/features/notfoundpage/notfoundpage.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-notfoundpage',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule, TranslateModule],
   templateUrl: './notfoundpage.component.html',
   styleUrl: './notfoundpage.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
