13c5e9b5b2b7e8d4d39e74c9ca424f46a710a940
Miriam <miri@paf1.com>
2025-03-15 17:53:30 +0100

    Creación de componente y ruta para landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,10 @@ import { LegalNoticeComponent } from './policy/legal-notice/legal-notice.compone
 import { CookiesComponent } from './policy/cookies/cookies.component';
 import { PrivacyComponent } from './policy/privacy/privacy.component';
 import { NotfoundpageComponent } from './features/notfoundpage/notfoundpage.component';
+import { LandingPageComponent } from './layout/landing-page/landing-page.component';
 
 export const routes: Routes = [
+  { path: 'landing', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'legal-notice', component: LegalNoticeComponent },
