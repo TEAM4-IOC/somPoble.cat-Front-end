6cc2334911adf912e18e70056bf90da97e92fb6d
Miriam <miri@paf1.com>
2025-03-16 13:06:26 +0100

    cambios estilo

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,13 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { I18nService } from '../../core/services/i18n.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [RouterModule, CommonModule, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -12,7 +16,7 @@ export class HeaderComponent implements AfterViewInit {
   language: string | null = localStorage.getItem('language');
   isMenuOpen = false;
 
-  constructor(private i18nService: I18nService) {}
+  constructor(private i18nService: I18nService, private cdr: ChangeDetectorRef) {}
 
   ngAfterViewInit() {
     const langBtn = document.querySelector(".lang-btn") as HTMLElement;
@@ -38,26 +42,22 @@ export class HeaderComponent implements AfterViewInit {
     if (langEs) {
       langEs.addEventListener("click", () => {
         this.language = "es";
-        setTimeout(() => {
-          if (this.language) {
-            this.i18nService.changeLanguage(this.language);
-            langSelector.classList.add("d-none");
-            langBtn.classList.remove("active"); 
-          }
-        }, 0);
+        localStorage.setItem('language', this.language);
+        this.i18nService.changeLanguage(this.language);
+        this.cdr.detectChanges(); 
+        langSelector.classList.add("d-none");
+        langBtn.classList.remove("active"); 
       });
     }
 
     if (langCat) {
       langCat.addEventListener("click", () => {
         this.language = "cat";
-        setTimeout(() => {
-          if (this.language) {
-            this.i18nService.changeLanguage(this.language);
-            langSelector.classList.add("d-none");
-            langBtn.classList.remove("active"); 
-          }
-        }, 0);
+        localStorage.setItem('language', this.language);
+        this.i18nService.changeLanguage(this.language);
+        this.cdr.detectChanges(); 
+        langSelector.classList.add("d-none");
+        langBtn.classList.remove("active"); 
       });
     }
   }
