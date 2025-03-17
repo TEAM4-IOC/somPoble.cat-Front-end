bb38b8661f2175c5bc865c9810b8a13fc10f7ef2
Daniel <danideveloperweb@gmail.com>
2025-03-17 14:34:10 +0100

    Traduccion

--- src/app/features/empresa-form/empresa-form.component.ts
+++ src/app/features/empresa-form/empresa-form.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-empresa-form',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, TranslateModule, RouterModule],
   templateUrl: './empresa-form.component.html',
   styleUrls: ['./empresa-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
