33ab83c25e67e6cbb03a1beb5091f7323cdc81cd
Miriam <miri@paf1.com>
2025-03-20 14:53:04 +0100

    reestructuración policy + 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
-import { LegalNoticeComponent } from './policy/legal-notice/legal-notice.component';
-import { CookiesComponent } from './policy/cookies/cookies.component';
-import { PrivacyComponent } from './policy/privacy/privacy.component';
-import { NotfoundpageComponent } from './features/notfoundpage/notfoundpage.component';
+import { LegalNoticeComponent } from './shared/policy/legal-notice/legal-notice.component';
+import { CookiesComponent } from '../app/shared/policy/cookies/cookies.component';
+import { PrivacyComponent } from './shared/policy/privacy/privacy.component';
+import { NotfoundpageComponent } from './shared/notfoundpage/notfoundpage.component';
 import { LandingPageComponent } from './features/landing-page/landing-page.component';
 import { EmpresaFormComponent } from './features/empresa-form/empresa-form.component';
 import { EditComponent } from './features/edit/edit.component';
@@ -15,7 +15,7 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'empresa-form', component: EmpresaFormComponent },
   { path: 'edit', component: EditComponent },
-  { path: 'legal-neotice', component: LegalNoticeComponent },
+  { path: 'legal-notice', component: LegalNoticeComponent },
   { path: 'cookies', component: CookiesComponent },
   { path: 'privacy', component: PrivacyComponent },
   { path: 'not-found', component: NotfoundpageComponent },
--- src/app/shared/policy/legal-notice/legal-notice.component.ts
+++ src/app/shared/policy/legal-notice/legal-notice.component.ts
@@ -2,18 +2,18 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-import { LoadingService } from '../../core/services/loading.service';
+import { LoadingService } from '../../../core/services/loading.service';
 import { NgxSpinnerModule } from 'ngx-spinner';
 
 @Component({
-  selector: 'app-privacy',
+  selector: 'app-legal-notice',
   standalone: true,
   imports: [RouterModule, CommonModule, TranslateModule, NgxSpinnerModule],
-  templateUrl: './privacy.component.html',
-  styleUrl: './privacy.component.scss',
+  templateUrl: './legal-notice.component.html',
+  styleUrl: './legal-notice.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PrivacyComponent implements OnInit {
+export class LegalNoticeComponent implements OnInit {
   constructor(public loadingService: LoadingService) {}
 
   ngOnInit() {
