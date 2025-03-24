fd776ddff4c9537a200cd5fd3883deada80140e9
Miriam <miri@paf1.com>
2025-03-24 08:28:25 +0100

    preparación formulario servicios

---
+++ src/app/features/services-form/services-form.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './services-form.component.html',
+  styleUrl: './services-form.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ServicesFormComponent {
+
+}
