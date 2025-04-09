192db1e6128220c999b8a31185eb96dff24f03b2
Daniel <danideveloperweb@gmail.com>
2025-04-09 20:54:39 +0200

    vista detalle y servicio acabado

--- src/app/features/show-services/show-services.component.ts
+++ src/app/features/show-services/show-services.component.ts
@@ -4,13 +4,15 @@ import { ServicioData } from '../../core/models/ServicioData.interface';
 import { ApiService } from '../../core/services/api.service';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-show-services',
   standalone: true,
   imports: [
     CommonModule,
-    TranslateModule
+    TranslateModule,
+    RouterModule,
   ],
   templateUrl: './show-services.component.html',
   styleUrls: ['./show-services.component.scss']
@@ -18,11 +20,20 @@ import { TranslateModule } from '@ngx-translate/core';
 export class ShowServicesComponent implements OnInit {
   services$: Observable<ServicioData[]>;
 
-  constructor(private apiService: ApiService) {
+  constructor(private apiService: ApiService, private router: Router) {
     this.services$ = new Observable<ServicioData[]>();
   }
 
   ngOnInit(): void {
     this.services$ = this.apiService.getServicios();
+  }
+
+  onSelectService(service: ServicioData): void {
+    this.router.navigate(['/service-detail', service.idServicio, service.identificadorFiscal]);
+  }
+
+
+  trackByServiceName(index: number, service: ServicioData): string {
+    return service.nombre;
   }
 }
